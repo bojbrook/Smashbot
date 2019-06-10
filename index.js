3f28aca0abecfd7b9ced0993871c2f5c4cf4c6fb
Bowen Brooks <bowenjbrooks@gmail.com>
2019-06-10 11:05:18 -0700

    got the basic of the smashbot working

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
+const fs = require('fs');
+const taunts = require('./quotes');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+fs.readdir('./events/', (err, files) => {});
 
-const SMASH_KEYWORD = "smash"
+fs.readdir('./events/', (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`);
+  });
+});
+
+
+const SMASH_KEYWORD = "smash";
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(taunts);
 })
 
 client.on('message', msg => {
@@ -15,8 +26,11 @@ client.on('message', msg => {
   // }
   console.log(`Author:  ${msg.author.tag} bot: ${client.user.tag}`)
   if (msg.author.tag !== client.user.tag){
-    // if(msg.content )
-    msg.reply(msg.content )
+    var content_lower_case = msg.content.toLowerCase()
+    if(content_lower_case.includes(SMASH_KEYWORD))
+      var index = Math.floor(Math.random() * taunts.length);
+      console.log(`index: ${index} message: ${taunts[index]}`);
+      msg.reply(taunts[index]);
   }
 })
 
