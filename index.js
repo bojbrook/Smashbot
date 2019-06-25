9057dacc26eea341c160638760ed9b8bda4dae90
Bowen Brooks <bowenjbrooks@gmail.com>
2019-06-25 11:50:46 -0700

    updated the index

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
 require('dotenv').config();
-const fs = require('fs');
 const taunts = require('./quotes');
+
+const taunt = require('./static/quotes.json');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-fs.readdir('./events/', (err, files) => {});
-
-fs.readdir('./events/', (err, files) => {
-  files.forEach(file => {
-    const eventHandler = require(`./events/${file}`);
-  });
-});
-
 
 const SMASH_KEYWORD = "smash";
+const PICTURE = "!p";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(taunts);
+  console.log(taunt);
+  console.log(client.users.keys().next());
+  var map = client.users.keys();
+  var tp = map.next()
+  console.log(tp)
+
+  for(var index = 0; index < client.users.keys().length; index++){
+    console.log(`Doing this for loop: ${client.users.keys()[index]}`)
+  }
+
 })
 
 client.on('message', msg => {
-  // if (msg.content === 'ping' && msg.author.tag !== client.user.tag) {
-  //   msg.reply('Pong!')
+  
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop.
+  if(msg.author.bot) return;
+  var content_lower_case = msg.content.toLowerCase();
+  
+  // // insult command
+  // if (content_lower_case[0] ==='!' && content_lower_case.includes('insult')){
+  //   console.log("got to this command");
+  //   if(msg.isMemberMentioned){
+  //     console.log(`User was mentioned`)
+  //   }else{
+  //     console.log('no user was mentioned')
+  //   }
   // }
-  console.log(`Author:  ${msg.author.tag} bot: ${client.user.tag}`)
-  if (msg.author.tag !== client.user.tag){
-    var content_lower_case = msg.content.toLowerCase()
-    if(content_lower_case.includes(SMASH_KEYWORD))
-      var index = Math.floor(Math.random() * taunts.length);
-      console.log(`index: ${index} message: ${taunts[index]}`);
-      msg.reply(taunts[index]);
+
+  console.log(`Author:  ${msg.author.tag} bot: ${client.user.tag}`);
+  console.log("Content: " + content_lower_case);
+  
+  //smash command
+  if(content_lower_case.includes(SMASH_KEYWORD)){
+    console.log("I'm just here for the pictures")
+    var index = Math.floor(Math.random() * taunt.length);
+    msg.channel.send(taunt[index]['text'], {files: [taunt[index]['img']]})
   }
+
 })
+
 
 client.login(process.env.BOT_TOKEN)
