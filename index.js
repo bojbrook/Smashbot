f7c36a70a9f2c8b2a471769d81efcde8587953f1
Bowen Brooks <bowenjbrooks@gmail.com>
2020-02-08 11:02:40 -0800

    Added the F in chat

--- index.js
+++ index.js
@@ -10,14 +10,21 @@ const { createLogger, format, transports } = require('winston');
 const SMASH_KEYWORD = "smash";
 const CATAN_KEYWORD = "catan";
 const LIST_SMASH_KEYWORD = "\list";
-const logger = createLogger({
-  level: 'info',
-  exitOnError: false,
-  format: format.json(),
-  transports: [
-    new transports.File({ filename: "/var/log/smashbot/bot.log" }),
-  ],
-});
+//Code for logging in production
+if (process.env.ENV == "PROD"){
+  console.log("Running in production");
+  const logger = createLogger({
+    level: 'info',
+    exitOnError: false,
+    format: format.json(),
+    transports: [
+      new transports.File({ filename: "/var/log/smashbot/bot.log" }),
+    ],
+  });
+}else{
+  console.log("Running in Development");
+}
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -25,26 +32,35 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop.
   if(msg.author.bot) return;
   var content_lower_case = msg.content.toLowerCase();
 
   console.log(`Author:  ${msg.author.tag} bot: ${client.user.tag}`);
-  // console.log("Content: " + content_lower_case);
-  
+
+  //For an F in chat
+  if(content_lower_case == "f"){
+    console.log('Big f in chat');
+    msg.channel.send(smash[12]['text'],  {files: [smash[12]['img']]});
+    // var index = Math.floor(Math.random() * smash.length);
+    // msg.channel.send(smash[index]['text'], {files: [smash[index]['img']]});
+  }
+
   //smash command
   if(content_lower_case.includes(SMASH_KEYWORD)){
     	var index = Math.floor(Math.random() * smash.length);
-    	msg.channel.send(smash[index]['text'], {files: [smash[index]['img']]})
-    	logger.info(smash[index]['text'],{author: `${msg.author.tag}`, file: `${[smash[index]['img']]}` });
+      msg.channel.send(smash[index]['text'], {files: [smash[index]['img']]});
+      //Datadog loging 
+      if (process.env.ENV == "PROD")
+    	  logger.info(smash[index]['text'],{author: `${msg.author.tag}`, file: `${[smash[index]['img']]}` });
   }
   if (content_lower_case.includes(LIST_SMASH_KEYWORD)){
   	console.log("Listing all the possibilites");
-	console.log(`Length: ${smash.length}`);
-	var i;
-	for (i = 0; i < smash.length; i++){
-		msg.channel.send(smash[i]['text'], {files: [smash[i]['img']]})
+    console.log(`Length: ${smash.length}`);
+    var i;
+    for (i = 0; i < smash.length; i++){
+      msg.channel.send(smash[i]['text'], {files: [smash[i]['img']]})
 	}
   }
   else if(content_lower_case.includes(CATAN_KEYWORD)){
